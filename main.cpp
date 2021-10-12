b1832721c9ae10a2e33a43d417acd230110cde84
mamingkun <mamingkun3@gmail.com>
2021-10-12 18:09:15 +0800

    提交 硬解码时 查看设备支持颜色格式 硬解码 提前分配内存

--- main.cpp
+++ main.cpp
@@ -6,12 +6,17 @@ extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavutil/hwcontext.h>
 #include <libavutil/pixdesc.h>
+#include <libavutil/imgutils.h>
 }
 #define INBUF_SIZE 4096
 const char* hw_type = "cuda";
 // Available device types: cuda dxva2 qsv d3d11va opencl vulkan
 static AVBufferRef *hw_device_ctx = NULL;
 static enum AVPixelFormat hw_pix_fmt;
+
+// use to copy to hw device, these points have not free
+AVFrame *g_sw_frame = nullptr;
+uint8_t *g_frame_buffer = nullptr;
 
 class CClock {
 private:
@@ -45,7 +50,8 @@ static enum AVPixelFormat get_hw_format(AVCodecContext *ctx, const enum AVPixelF
     const enum AVPixelFormat *p;
     for (p = pix_fmts; *p != -1; p++) {
         if (*p == hw_pix_fmt) {
-            std::cout << "get hw_format " << av_get_pix_fmt_name(*pix_fmts) << std::endl;
+            std::cout << "get hw_format " << av_get_pix_fmt_name(*pix_fmts)
+            << " hw_pix_fmt: " << av_get_pix_fmt_name(hw_pix_fmt) << std::endl;
             return *p;
         }
     }
@@ -59,6 +65,7 @@ bool hw_init(AVCodecContext *dec_ctx){
     type = av_hwdevice_find_type_by_name(hw_type);
     std::cout << "##################av_hwdevice_find_type_by_name:" << type;
     type = AV_HWDEVICE_TYPE_CUDA;
+    // 查看支持设备类型
     if (type == AV_HWDEVICE_TYPE_NONE){
         fprintf(stderr, "Device type %s is not supported.\n", hw_type);
         fprintf(stderr, "Available device types:");
@@ -69,16 +76,19 @@ bool hw_init(AVCodecContext *dec_ctx){
         return false;
     }
 
+    const AVCodecHWConfig *config = nullptr;
     for (int i = 0; ; i++){
-        const AVCodecHWConfig *config = avcodec_get_hw_config(dec_ctx->codec, i);
+        config = avcodec_get_hw_config(dec_ctx->codec, i);
         if (!config) {
             fprintf(stderr, "Decode %s does not surport device type %s.\n", dec_ctx->codec->name,
                     av_hwdevice_get_type_name(type));
             return false;
         }
 
+        std::cout << "#######################av_get_pix_fmt_name: " << dec_ctx->codec->name << " " << std::endl;
         if (config->methods & AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX && config->device_type == type) {
             hw_pix_fmt = config->pix_fmt;
+            std::cout << "######################AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX " << av_get_pix_fmt_name(hw_pix_fmt) << "  end"<< std::endl;
             break;
         }
     }
@@ -91,6 +101,28 @@ bool hw_init(AVCodecContext *dec_ctx){
         return false;
     }
     dec_ctx->hw_device_ctx = av_buffer_ref(hw_device_ctx);
+
+    AVHWFramesConstraints * hw_frame_constrains = av_hwdevice_get_hwframe_constraints(av_buffer_ref(hw_device_ctx), config);
+    std::cout << "min-width: " << hw_frame_constrains->min_width << " min-height: " << hw_frame_constrains->min_height << std::endl;
+    std::cout << "max-width: " << hw_frame_constrains->max_width << " max-height: " << hw_frame_constrains->max_height << std::endl;
+
+    AVPixelFormat* index = hw_frame_constrains->valid_hw_formats;
+    std::cout << "hw_frame_constrains->valid_hw_formats: ";
+    while (*index != AV_PIX_FMT_NONE){
+        std::cout << av_get_pix_fmt_name(*index) << " ";
+        index++;
+    }
+    std::cout << std::endl;
+    std::cout << "###############AV_PIX_FMT_GBRAP " << AV_PIX_FMT_GBRAP << std::endl;
+
+    index = hw_frame_constrains->valid_sw_formats;
+    std::cout << "hw_frame_constrains->valid_sw_formats: ";
+    while (*index != AV_PIX_FMT_NONE){
+        std::cout << av_get_pix_fmt_name(*index) << "(" << *index << ")"<<" ";
+        index++;
+    }
+    std::cout << std::endl;
+
     return true;
 }
 
@@ -102,16 +134,17 @@ void hw_decode(AVCodecContext *dec_ctx, AVPacket *pkt) {
         return;
     }
 
-    AVFrame *frame, *sw_frame = NULL;
-    AVFrame *tmp_frame = NULL;
+    AVFrame *frame;
+    AVFrame *tmp_frame = nullptr;
+
     while (true){
         frame = av_frame_alloc();
-        sw_frame = av_frame_alloc();
+//        g_sw_frame = av_frame_alloc();
+//        g_sw_frame->format = AV_PIX_FMT_NV12;
 
         ret = avcodec_receive_frame(dec_ctx, frame);
         if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
             av_frame_free(&frame);
-            av_frame_free(&sw_frame);
             return;
         }else if (ret < 0){
             std::cout << "Error during decoding" << std::endl;
@@ -119,19 +152,40 @@ void hw_decode(AVCodecContext *dec_ctx, AVPacket *pkt) {
         }
 
         if (frame->format == hw_pix_fmt) {
-            if ((ret = av_hwframe_transfer_data(sw_frame, frame, 0)) < 0) {
+            if (g_sw_frame == nullptr) {
+                g_sw_frame = av_frame_alloc();
+                AVPixelFormat dst_pix_fmt = AV_PIX_FMT_NV12;
+                int buffer_size = av_image_get_buffer_size(dst_pix_fmt, frame->width, frame->height, 1);
+                std::cout << "#####################frame->width: " << frame->width
+                << "  frame->height: " << frame->height
+                << "  format:" << frame->format
+                << "  format name:" << av_get_pix_fmt_name(static_cast<AVPixelFormat>(frame->format))
+                << "  frame buffer size:" << buffer_size << std::endl;
+                g_frame_buffer = (uint8_t*)av_malloc(buffer_size);
+                g_sw_frame->format = dst_pix_fmt;
+                av_image_fill_arrays(g_sw_frame->data,
+                                     g_sw_frame->linesize,
+                                     g_frame_buffer,
+                                     dst_pix_fmt,
+                                     frame->width,
+                                     frame->height,
+                                     1);
+            }
+
+            if ((ret = av_hwframe_transfer_data(g_sw_frame, frame, 0)) < 0) {
                 std::cout << "Error transferring the data to system memory" << std::endl;
                 return;
             }
             else{
-                tmp_frame = sw_frame;
+                tmp_frame = g_sw_frame;
             }
         }else{
             tmp_frame = frame;
         }
-
+        std::cout << "tmp_frame fmt: " << av_get_pix_fmt_name(static_cast<AVPixelFormat>(tmp_frame->format)) << std::endl;
+        printf("####################points address: %p \n", tmp_frame->data[0]);
         av_frame_free(&frame);
-        av_frame_free(&tmp_frame);
+//        av_frame_free(&g_sw_frame);
     }
 }
 
@@ -159,7 +213,7 @@ soft_decode_err:
 }
 
 int main() {
-    bool enable_hwdecode = false;
+    bool enable_hwdecode = true;
     const char *filename = "D:\\project\\soft_ffmpeg_decode\\raw_data.h264";
 
     uint8_t inbuf[INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
